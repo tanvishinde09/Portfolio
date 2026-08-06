171b069beaf8c794e5e86475ac4f35a61d55d6e0
tanvishinde09 <tanvishinde34665@gmail.com>
2026-08-07 00:22:24 +0530

    Create App component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Achievements from "./components/Achievements";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Achievements />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
